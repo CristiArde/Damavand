af18afa7a8d0820766b8650c54c3a2e36524c310
GurkomalSRao <gsrao@hotmail.ca>
2017-04-07 12:29:01 -0400

    update

--- content/getItems_content.php
+++ content/getItems_content.php
@@ -7,46 +7,58 @@
     require 'connection.php';
     $sql="SELECT supplierName, o.projectID, phaseID, i.orderID , itemName, unitCost, quantity FROM items i INNER JOIN orders o on i.orderID = o.orderID INNER JOIN supplier s ON i.supplierID = s.supplierID WHERE o.projectID = '".$id."' GROUP BY supplierName,o.projectID, i.orderID , itemName, unitCost, quantity HAVING o.projectID = '".$id."'";
 
-    $result = mysqli_query($connection,$sql);   
-    echo '<h1>Damavand Construction INC.</h1>';
-    echo '<h3>Items Ordered for Project '.$id.'</h3>';  
+    $result = mysqli_query($connection,$sql);
+    $check = mysqli_num_rows($result);
 
-    echo "<ul>
-            <li>
-                <button  onclick=\"location.href ='Welcome.php';\" class=\"button button2\">Home</button>
-                >>
-                <button onclick=\"location.href ='projectDetails.php';\" class=\"button button2\">Project " .$id. "  Details</button> 
-                >>
-                <button onclick=\"location.href ='getOrders.php?id=".$id."&type=Project';\" class=\"button button2\">Phase orders</button>
-                >>
-                 <button onclick='window.location.reload(true);' class=\"button button2\">Ordered Items</button>  
-            </li>
-        </ul>";
+    if($check != 0){   
+        echo '<h1>Damavand Construction INC.</h1>';
+        echo '<h3>Items Ordered for Project '.$id.'</h3>';  
+
+        echo "<ul>
+                <li>
+                    <button  onclick=\"location.href ='Welcome.php';\" class=\"button button2\">Home</button>
+                    >>
+                    <button onclick=\"location.href ='projectDetails.php';\" class=\"button button2\">Project " .$id. "  Details</button> 
+                    >>
+                    <button onclick=\"location.href ='getOrders.php?id=".$id."&type=Project';\" class=\"button button2\">Phase orders</button>
+                    >>
+                     <button onclick='window.location.reload(true);' class=\"button button2\">Ordered Items</button>  
+                </li>
+            </ul>";
 
 
-    echo 
-    "<table id='items-table'>
-        <tr>
-            <th>Phase Number</th>
-            <th>Order Number</th>
-            <th>Supplier</th>
-            <th>Item Name</th>
-            <th>Cost Per Unit</th>
-            <th>Quantity</th>
-        </tr>";
-    $count = 0;
-    $supplierName = "";
-    while($row = mysqli_fetch_array($result)) {
-        echo "<tr>";
-        echo "<td>" . $row['phaseID'] . "</td>";
-        echo "<td>" . $row['orderID'] . "</td>";
-        echo "<td>" . $row['supplierName'] . "</td>";
-        echo "<td>" . $row['itemName'] . "</td>";
-        echo "<td>$" . $row['unitCost'] . "</td>";
-        echo "<td>" . $row['quantity'] . "</td>";
-       $count += 1;
+        echo 
+        "<table id='items-table'>
+            <tr>
+                <th>Phase Number</th>
+                <th>Order Number</th>
+                <th>Supplier</th>
+                <th>Item Name</th>
+                <th>Cost Per Unit</th>
+                <th>Quantity</th>
+            </tr>";
+        $count = 0;
+        $supplierName = "";
+        while($row = mysqli_fetch_array($result)) {
+            echo "<tr>";
+            echo "<td>" . $row['phaseID'] . "</td>";
+            echo "<td>" . $row['orderID'] . "</td>";
+            echo "<td>" . $row['supplierName'] . "</td>";
+            echo "<td>" . $row['itemName'] . "</td>";
+            echo "<td>$" . $row['unitCost'] . "</td>";
+            echo "<td>" . $row['quantity'] . "</td>";
+           $count += 1;
+        }
+        echo "</table>";
     }
-    echo "</table>";
+    else
+    {
+        //TODO
+        /*SHOW NAVIGATION BAR
+        SHOW BACK BUTTON
+        SHOW ADD ORDERS BUTTON IF PROJECT/PHASE/TASK NOT COMPLETED*/
+        echo " No Items ";
+    }
     mysqli_close($connection);
     ?>
 </div>
---
+++ updateFunction
@@ -0,0 +1,70 @@
+<head>
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<div id='items-main' class='center'>
+    <?php
+
+    /*=================================================================
+            FILE TO MODIFY PROJECTS, ORDERS AND TASKS
+    =================================================================*/
+    require 'connection.php';
+    session_start();
+ 
+    $type = $_POST['type'];
+    //$phaseID = $_POST['phaseID'];
+
+    if($type=='Project'){
+        $projectID = $_POST['projectID'];
+        $projectName = $_POST['projectName'];;
+        $projectManagerID = $_POST['projectManagerID'];;
+        $customerID = $_POST['customerID'];;
+        $startDate = $_POST['startDate'];;
+        $endDate = $_POST['endDate'];;
+        $siteAddress = $_POST['siteAddress'];;
+        $status = $_POST['status'];;
+        $estimatedCost = $_POST['estimatedCost'];;
+        $actualCost = $_POST['actualCost'];;
+
+        $sql = "UPDATE project SET projectID = '".$projectID."', projectName = '".$projectName."', projectManagerID = '".$projectManagerID."', customerID = '".$customerID."', startDate = '".$startDate."', endDate = '".$endDate."', siteAddress = '".$siteAddress."', status = '".$status."', estimatedCost = '".$estimatedCost."', actualCost = '".$actualCost."' WHERE projectID ='".$projectID."'";
+    }else if($type=='Order'){
+        $orderID = $_POST['orderID'];
+        $projectID = $_POST['projectID'];
+        $phaseID = $_POST['phaseID'];
+        $taskID = $_POST['taskID'];
+        $supplierID = $_POST['supplierID'];
+        $totalCost = $_POST['totalCost'];
+        $orderDate = $_POST['orderDate'];
+        $estimatedDeliveryDate = $_POST['estimatedDeliveryDate'];
+
+        $sql = "UPDATE orders SET orderID = ".$orderID.", projectID = ".$projectID.", phaseID = ".$phaseID.", taskID = ".$taskID.", supplierID = ".$supplierID.", totalCost = ".$totalCost.", orderDate  = ".$orderDate.", estimatedDeliveryDate = ".$estimatedDeliveryDate." WHERE CONCAT(taskID,phaseID,projectID) = '".$taskID.$phaseID.$projectID."'";
+
+
+    }else if($type=='Task'){
+        //primary key =  concatination of projectID,phaseID & taskID
+    	
+    	$projectID = $_POST['projectID'];
+        $projectName = $_POST['projectName'];
+        $projectManagerID = $_POST['projectManagerID'];
+        $customerID = $_POST['customerID'];
+        $actualStartDate = $_POST['actualStartDate'];
+        $estimatedStartDate = $_POST['estimatedStartDate'];
+        $actualStartDate = $_POST['actualEndDate'];
+        $estimatedStartDate = $_POST['estimatedEndDate'];
+        $status = $_POST['status'];
+        $actualCost = $_POST['actualCost'];
+        $estimatedCost = $_POST['estimatedCost'];
+
+
+        $sql = "UPDATE task taskID = '".."', phaseID = '".."', projectID = '".."', taskName = '".."', estimatedStartDate = '".."', actualStartDate = '".."', estimatedCost = '".."', estimatedEndDate = '".."', actualEndDate = '".."', actualCost = '".."', status SET WHERE = CONCAT(taskID,phaseID,projectID) = '".$taskID.$phaseID.$projectID."'";
+    }
+    
+
+
+    $result = mysqli_query($connection,$sql);
+    mysqli_close($connection);
+    if($type == 'Task')
+        header('Location: /Damavand/task.php');
+    else
+        header('Location: /Damavand/Welcome.php');
+    ?>
+</div>
